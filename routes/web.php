7e9e7a698606a8e695b79fd6c4df65302565f755
inversemaha <mahajur88@gmail.com>
2019-07-09 01:28:18 +0600

    Test Done

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// SSLCOMMERZ Start
+Route::any('/pay', 'PublicSslCommerzPaymentController@index');
+Route::any('/success', 'PublicSslCommerzPaymentController@success');
+Route::POST('/fail', 'PublicSslCommerzPaymentController@fail');
+Route::POST('/cancel', 'PublicSslCommerzPaymentController@cancel');
+Route::POST('/ipn', 'PublicSslCommerzPaymentController@ipn');
+//SSLCOMMERZ END
+
+Route::any('/test', function (){
+    echo "Hello";
+});
